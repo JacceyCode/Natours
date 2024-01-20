f7694bc0c244281a92b5d207983ec7b85ab51707
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2024-01-20 01:44:32 +0100

    uploading data to mondoDB database using express API

--- server.js
+++ server.js
@@ -17,44 +17,8 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => {
-    console.log('DB connection successful!');
-
-    const tourSchema = new mongoose.Schema({
-      name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true,
-      },
-      rating: {
-        type: Number,
-        default: 4.5,
-      },
-      price: {
-        type: Number,
-        required: [true, 'A tour must have a price'],
-      },
-    });
-
-    const Tour = mongoose.model('Tour', tourSchema);
-
-    const testTour = new Tour({
-      name: 'The Park Camper',
-      price: 997,
-    });
-
-    testTour
-      .save()
-      .then((doc) => {
-        console.log(doc);
-      })
-      .catch((err) => {
-        console.log('Error 💥:', err);
-      });
-  })
-  .catch((err) => {
-    console.log('DB connection failed 💥💥', err);
-  });
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.log('DB connection failed 💥💥', err));
 
 // START SERVER
 const port = process.env.PORT || 3000;
