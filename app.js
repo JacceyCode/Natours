a6f9130fb0ee34cf187448c59dd3dc4f67c916e5
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2024-02-17 12:53:59 +0100

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -24,6 +25,16 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARE
+// Implement CORS
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com',
+//   }),
+// );
+
+app.options('*', cors());
+
 // To display static files(HTML)
 app.use(express.static(path.join(__dirname, 'public')));
 
