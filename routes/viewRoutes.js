2b4dbae24aea8cf945fd5872d8241c6c625ce086
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2024-02-17 16:12:43 +0100

    checkout success message alert

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,12 +6,15 @@ const {
   getAccount,
   updateUserData,
   getMyTours,
+  alerts,
   //   getSignUpForm,
 } = require('../controllers/viewsController');
 
 const { isLoggedIn, protect } = require('../controllers/authController');
 
 const router = express.Router();
+
+router.use(alerts);
 
 router.get('/', isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
